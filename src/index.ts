0b3c1132aa2611722ed2b082b3ddeb389cfd322f
rherbawy5@gmail.com <202111979@ua.edu.lb>
2025-03-10 03:23:36 +0200

    parsing engine

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,10 @@
-import { FinanceCalculator, ItemValidator, MaxPriceValidator, OrderManagement, PriceValidator, Validator } from './app';
-import logger from './util/logger';
+import logger from "./util/logger";
+import { readCSVFile } from "./util/parser";
 
-  const orders = [
-    { id: 1, item: "Sponge", price: 15 },
-    { id: 2, item: "Chocolate", price: 20 },
-    { id: 3, item: "Fruit", price: 18 },
-    { id: 4, item: "Red Velvet", price: 25 },
-    { id: 5, item: "Coffee", price: 8 },
-  ];
 
-  const rules = [
-    new PriceValidator(),
-    new ItemValidator(),
-    new MaxPriceValidator()
-];
+async function main() {
+    const data = await readCSVFile("src/data/cake orders.csv");
+    data.forEach((row) => logger.info(row));
+}
 
-  const OrderManager = new OrderManagement(new Validator(rules), new FinanceCalculator());
-  for (const order of orders) {
-    OrderManager.addOrder(order.item, order.price);
-  }
-  
-  // Adding a new order directly
-  const newItem = "Marble";
-  const newPrice = 22;
-
-  const start = Date.now();
-  OrderManager.addOrder(newItem, newPrice);
-  const end = Date.now();
-  logger.info(`Order added in ${end - start}ms`, { item: newItem, price: newPrice });
-  logger.info("Orders after adding a new order: %o" ,OrderManager.getOrders());
-  
-  // Calculate Total Revenue directly
-  logger.info("Total Revenue:" + OrderManager.getTotalRevenue());
-  
-  // Calculate Average Buy Power directly
-  logger.info("Average Buy Power:" + OrderManager.getBuyPower());
-  
-  // Fetching an order directly
-  const fetchId = 2;
-  const fetchedOrder = OrderManager.getOrder(fetchId);
-  logger.info("Order with ID 2: %o" , fetchedOrder);
-  
-  // Attempt to fetch a non-existent order
-  const nonExistentId = 10;
-  const nonExistentOrder = OrderManager.getOrder(nonExistentId);
-  logger.info("Order with ID 10 (non-existent):" + nonExistentOrder);
+main();
---
+++ src/util/parser.ts
@@ -0,0 +1,35 @@
+import {promises as fs} from 'fs';
+import {parse as csvParse } from 'csv-parse';
+import {stringify as csvStringify} from 'csv-stringify';
+
+export async function readCSVFile(filePath: string, includeHeader: boolean = false): Promise<string[][]> {
+    try {
+        const fileContent = await fs.readFile(filePath, 'utf-8');
+        return new Promise((resolve, reject) => {
+            csvParse(fileContent, {
+                trim:true,
+                skip_empty_lines: true
+            }, (err, records:string[][]) => {
+                if(err) reject(err);
+                if(!includeHeader) records.shift();
+                resolve(records);
+            });
+        });
+    } catch (error) {
+        throw new Error(`error reading csv file: ${error}`);
+    }
+};
+
+export async function writeCSVFile(filePath: string, data: string[][]): Promise<void> {
+    try {
+        const csvContent = await new Promise<string>((resolve, reject) => {
+            csvStringify(data, (err, output) => {
+                if (err) reject(err);
+                resolve(output);
+            });
+        });
+        await fs.writeFile(filePath, csvContent, 'utf-8');
+    } catch (error) {
+        throw new Error(`error writing csv file: ${error}`);
+    };
+};
